d3afa8621f77bd1df36b23d85123f0d71909122e
imotro <139806892+imotro@users.noreply.github.com>
2023-09-04 13:44:58 -0500

    Update i.js

--- i.js
+++ i.js
@@ -1,6 +1,1 @@
-"use strict"; const express = require("express"); const Unblocker = require("unblocker"); const app = express(); 
-const unblocker = Unblocker({
-prefix: "/proxy/"
-}); 
-app.use(unblocker);
-app.get("/", (req, res) => res.sendFile(__dirname + '/index.html') ); 
+"use strict"; const express = require("express"); const Unblocker = require("unblocker"); const app = express(); const unblocker = Unblocker({prefix: "/proxy/"}); app.use(unblocker);app.get("/", (req, res) => res.sendFile(__dirname + '/index.html') ); 
