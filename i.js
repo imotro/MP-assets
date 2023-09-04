6ebe5f56c3229c483c6f6dfcaad91eb3443a8580
imotro <139806892+imotro@users.noreply.github.com>
2023-09-04 14:26:09 -0500

    Update i.js

--- i.js
+++ i.js
@@ -1,1 +1,8 @@
-"use strict";const express = require("express");const Unblocker = require("unblocker");const app = express();const unblocker = Unblocker({prefix: '/proxy/'});app.use(unblocker);app.get("/", (req, res) =res.sendFile(__dirname + '/index.html'));const port = process.env.PORT || 8080;app.listen(port).on("upgrade", unblocker.onUpgrade);console.log(`unblocker app live at http://localhost:${port}/`);
+"use strict";
+const express = require("express");
+const Unblocker = require("unblocker");
+const app = express();
+const unblocker = Unblocker({prefix: '/proxy/'});
+app.use(unblocker);app.get("/", (req, res) =res.sendFile(__dirname + '/index.html'));
+const port = process.env.PORT || 8080;app.listen(port).on("upgrade", unblocker.onUpgrade);
+console.log(`portable live at http://localhost:${port}/`);
